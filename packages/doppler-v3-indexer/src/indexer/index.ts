7a14f4925ff4da73e06eea32265566d118de3420
sogoiii <sogoiii@gmail.com>
2025-06-10 10:33:25 +0100

    Support the 3 networks we care about. AKA removed uniswap and ink.
    # Conflicts:
    #	packages/doppler-v3-indexer/ponder.config.ts
    #	packages/doppler-v3-indexer/src/indexer/blockHandlers.ts

--- packages/doppler-v3-indexer/src/indexer/index.ts
+++ packages/doppler-v3-indexer/src/indexer/index.ts
@@ -1,5 +1,5 @@
 export * from "./indexer-shared";
-export * from "./indexer-v3";
+// export * from "./indexer-v3";
 export * from "./indexer-v2";
 export * from "./indexer-v4";
 export * from "./indexer-v4-migrated";
